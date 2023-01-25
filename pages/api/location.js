7048aa57710b3f306db36e605c7e31117be3b2ed
Agrim <agrimsharma1714@gmail.com>
2023-01-26 01:15:17 +0530

    added smallcards, completed explore more section

---
+++ pages/api/location.js
@@ -0,0 +1,46 @@
+export default function handler(req, res) {
+    res.status(200).json([
+        {
+            "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Delhi",
+            "distance": "4-hour drive"
+        },
+        {
+            "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Mumbai",
+            "distance": "4-hour drive"
+        },
+        {
+            "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Bengluru",
+            "distance": "4-hour drive"
+        },
+        {
+                        "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Hydrabad",
+            "distance": "4-hour drive"
+        },
+        {
+                        "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Chhapra",
+            "distance": "4-hour drive"
+        },
+        {
+                        "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Bathinda",
+            "distance": "4-hour drive"
+        },
+        {
+                        "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Vikram Kaituka",
+            "distance": "4-hour drive"
+        },
+        {
+                        "img": "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+            "location": "Dahiyawan Tola",
+            "distance": "4-hour drive"
+        }
+        
+    ])
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Head from "next/head";
 import Banner from "@/components/Banner";
+import SmallCard from "@/components/SmallCard";
 
 export default function Home({ exploreData }) {
   return (
@@ -18,9 +19,17 @@ export default function Home({ exploreData }) {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby!</h2>
 
           {/* Pulling data from server- API ENDPOINTS */}
-          {exploreData?.map(item => (
-            <h1>{item.location}</h1>
+          {/* The "?" below after exploreData denotes to handle any error, like if there is no exploreData */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xxl:grid-cols-4">
+          {exploreData?.map((item) => (
+            <SmallCard
+              key={item.img}
+              img={item.img}
+              distance={item.distance}
+              location={item.location}
+            />
           ))}
+          </div>
         </section>
       </main>
     </>
@@ -28,7 +37,7 @@ export default function Home({ exploreData }) {
 }
 
 export async function getStaticProps() {
-  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+  const exploreData = await fetch("http://localhost:3000/api/location").then(
     (res) => res.json()
   );
 
