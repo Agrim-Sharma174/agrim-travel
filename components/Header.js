d467a475fbfc9938410ba04a9ec2bf1f4637eb4d
Agrim <agrimsharma1714@gmail.com>
2023-01-27 21:14:39 +0530

    added calender functionality

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,32 @@
 import Image from "next/image";
-import React from "react";
-import { AiOutlineSearch , AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
 import { BsGlobe } from "react-icons/bs";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { FiUsers } from "react-icons/fi";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+
+  // for date range
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+  // date range code till here
+
+const handleSelect = (ranges) => {
+  setStartDate(ranges.selection.startDate);
+  setEndDate(ranges.selection.endDate);
+}
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10 ">
       {/* left */}
@@ -15,7 +36,14 @@ function Header() {
 
       {/* Middle-Search*/}
       <div className="flex item-center md:border-2 rounded-full py-2 shadow-sm">
-        <input className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Search here" />{" "}
+        <input
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+          className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Search here"
+        />{" "}
         <AiOutlineSearch className=" hidden md:inline-flex h-8 w-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
@@ -23,13 +51,23 @@ function Header() {
       <div className="flex space-x-4 items-center justify-end text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
         <BsGlobe className="h-6" />
-        
+
         <div className="flex items-center space-x-2 border-2 p-2 py-1 rounded-full">
           <AiOutlineMenu className="h-6" />
           <HiOutlineUserCircle className="h-6" />
         </div>
-        
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-1">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={"FD5861"}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 }
