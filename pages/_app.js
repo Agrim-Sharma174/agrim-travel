d21cf381d33b6eb976228235d34dd0b323c8744e
Agrim <agrimsharma1714@gmail.com>
2023-01-30 20:51:25 +0530

    Search page completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,21 @@
 import '@/styles/globals.css'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: 'z-50',
+  delay: 100,
+});
+
+//Adding router
+//This will start the progress bar when the route changes
+Router.events.on("routeChangeStart", progress.start);
+//This will end the progress bar when the route changes
+Router.events.on("routeChangeComplete", progress.finish);
+//This will end the progress bar when the route changes any error occurs
+Router.events.on("routeChangeError", progress.finish);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
